9728bbe682aa4b204bc0e05a186f2fa02e8bb55b
IoIxD <alphaproject217@gmail.com>
2024-09-10 17:15:15 -0700

    rename to rs-systemtime

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-A better C* library for getting the date and time.
+Bindings for Rust's `SystemTime` to C/C++, because what's built in to both languages, sucks to use.
 
+# Usage
+
+C++ Usage:
+```cxx
+auto time = new SystemTime();
+auto elapsed = time->get_duration();
+auto secs = elapsed->as_secs_f32();
+printf("%1.1f\n", secs);
+```
+
+C Usage:
 ```c
 SystemTime *time = new_system_time();
-// ...
 Duration *elapsed = system_time_get_duration(time);
 float secs = duration_as_secs_f32(elapsed);
 printf("%1.1f\n", secs);
-// ...
 free(time);
 ```
 
-*it's actually just bindings to Rust's std::time::SystemTime
+# Installation
 
-It's used via Cmake:
+It can be imported via cmake:
 
 ```cmake
 FetchContent_Declare(
-    better_ctime
-    GIT_REPOSITORY "https://github.com/IoIxD/better_ctime.git"
+    rs_systemtime
+    GIT_REPOSITORY "https://github.com/IoIxD/rs_systemtime.git"
     GIT_PROGRESS TRUE
 )
-FetchContent_MakeAvailable(better_ctime)
-include_directories(${BETTER_CTIME_INCLUDE_DIRECTORY})
+FetchContent_MakeAvailable(rs_systemtime)
+include_directories(${RS_SYSTEMTIME_INCLUDE_DIRECTORY})
 ```
---
+++ include/better_ctime.hpp
@@ -0,0 +1,204 @@
+#ifndef __BETTER_CTIME_HPP
+#define __BETTER_CTIME_HPP
+
+#include <cstdint>
+#include <cstdlib>
+namespace internal {
+extern "C" {
+#include "better_ctime.h"
+}
+}  // namespace internal
+
+class Duration {
+ private:
+  internal::Duration* inner;
+  Duration(internal::Duration* dur) { this->inner = dur; };
+  ~Duration() { free(this->inner); };
+
+ public:
+  uint64_t as_micros() { return internal::duration_as_micros(this->inner); };
+
+  uint64_t as_millis() { return internal::duration_as_millis(this->inner); };
+
+  uint64_t as_nanos() { return internal::duration_as_nanos(this->inner); };
+
+  uint64_t as_secs() { return internal::duration_as_secs(this->inner); };
+
+  float as_secs_f32() { return internal::duration_as_secs_f32(this->inner); };
+
+  double as_secs_f64() { return internal::duration_as_secs_f64(this->inner); };
+
+  uint32_t subsec_micros() {
+    return internal::duration_subsec_micros(this->inner);
+  };
+
+  uint32_t subsec_millis() {
+    return internal::duration_subsec_millis(this->inner);
+  };
+
+  uint32_t subsec_nanos() {
+    return internal::duration_subsec_nanos(this->inner);
+  };
+
+  Duration* div_f32(float rhs) {
+    return new Duration(internal::duration_div_f32(this->inner, rhs));
+  };
+
+  Duration* div_f64(double rhs) {
+    return new Duration(internal::duration_div_f64(this->inner, rhs));
+  };
+
+  Duration* mul_f32(float rhs) {
+    return new Duration(internal::duration_mul_f32(this->inner, rhs));
+  };
+
+  Duration* mul_f64(double rhs) {
+    return new Duration(internal::duration_mul_f64(this->inner, rhs));
+  };
+
+  Duration* saturating_add(Duration* rhs) {
+    return new Duration(
+        internal::duration_saturating_add(this->inner, rhs->inner));
+  };
+
+  Duration* saturating_sub(Duration* rhs) {
+    return new Duration(
+        internal::duration_saturating_sub(this->inner, rhs->inner));
+  };
+
+  Duration* saturating_mul(uint32_t rhs) {
+    return new Duration(internal::duration_saturating_mul(this->inner, rhs));
+  };
+
+  Duration* checked_add(Duration* rhs) {
+    return new Duration(
+        internal::duration_checked_add(this->inner, rhs->inner));
+  };
+
+  Duration* checked_sub(Duration* rhs) {
+    return new Duration(
+        internal::duration_checked_sub(this->inner, rhs->inner));
+  };
+
+  Duration* checked_mul(uint32_t rhs) {
+    return new Duration(internal::duration_checked_mul(this->inner, rhs));
+  };
+
+  Duration* operator+(Duration& rhs) {
+#ifdef NDEBUG
+    return this->checked_add(&rhs);
+#else
+    return this->saturating_add(&rhs);
+#endif
+  };
+
+  Duration* operator-(Duration& rhs) {
+#ifdef NDEBUG
+    return this->checked_sub(&rhs);
+#else
+    return this->saturating_sub(&rhs);
+#endif
+  };
+  Duration* operator*(uint32_t rhs) {
+#ifdef NDEBUG
+    return this->checked_mul(rhs);
+#else
+    return this->saturating_mul(rhs);
+#endif
+  };
+  Duration* operator*(float rhs) {
+#ifdef NDEBUG
+    return this->mul_f32(rhs);
+#else
+    return this->mul_f32(rhs);
+#endif
+  };
+  Duration* operator*(double rhs) {
+#ifdef NDEBUG
+    return this->mul_f64(rhs);
+#else
+    return this->mul_f64(rhs);
+#endif
+  };
+
+  Duration* operator/(float rhs) {
+#ifdef NDEBUG
+    return this->div_f32(rhs);
+#else
+    return this->div_f32(rhs);
+#endif
+  };
+  Duration* operator/(double rhs) {
+#ifdef NDEBUG
+    return this->div_f64(rhs);
+#else
+    return this->div_f64(rhs);
+#endif
+  };
+  friend class SystemTime;
+};
+
+class SystemTime {
+  internal::SystemTime* inner;
+  SystemTime(internal::SystemTime* t) { this->inner = t; }
+
+ public:
+  SystemTime() { this->inner = internal::new_system_time(); }
+  ~SystemTime() { free(this->inner); };
+
+  static SystemTime get_epoch() {
+    return SystemTime(internal::system_time_get_epoch());
+  }
+
+  Duration* get_duration() {
+    return new Duration(internal::system_time_get_duration(this->inner));
+  };
+};
+
+/* uint64_t as_micros();
+
+uint64_t as_millis();
+
+uint64_t as_nanos();
+
+uint64_t as_secs();
+
+float as_secs_f32();
+
+double as_secs_f64();
+
+uint32_t subsec_micros();
+
+uint32_t subsec_millis();
+
+uint32_t subsec_nanos();
+
+struct Duration *div_f32(float rhs);
+
+struct Duration *div_f64(double rhs);
+
+struct Duration *mul_f32(float rhs);
+
+struct Duration *mul_f64(double rhs);
+
+struct Duration *saturating_add(struct
+Duration *dur);
+
+struct Duration *saturating_sub(struct
+Duration *dur);
+
+struct Duration *saturating_mul(uint32_t
+dur);
+
+struct Duration *checked_add(struct
+Duration *dur);
+
+struct Duration *checked_sub(struct
+Duration *dur);
+
+struct Duration *checked_mul(uint32_t
+dur);
+
+struct Duration *system_time_get_duration();*/
+
+#endif
